f6475913589aa56ab90be1285c80a5ba99040e73
hongsyong92 <ghdgus24@naver.com>
2021-08-28 00:12:06 +0900

    웹소켓 서버 설정

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
 
-console.log("server");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(3000);
+const handleListen = () => console.log(`서버 작동중 http://localhost:5000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(5000, handleListen);
